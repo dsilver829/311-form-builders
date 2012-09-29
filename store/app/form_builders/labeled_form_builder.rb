0548a9cf8fed1e98e3483ca467f5247e199e351e
David Silver <dsilver@candidatemetrics.com>
2012-09-28 17:30:57 -0700

    move error message display into form builder

--- store/app/form_builders/labeled_form_builder.rb
+++ store/app/form_builders/labeled_form_builder.rb
@@ -1,15 +1,28 @@
 class LabeledFormBuilder < ActionView::Helpers::FormBuilder
+  delegate :content_tag, :tag, to: :@template
   %w[text_field text_area password_field collection_select].each do |method_name|
     define_method(method_name) do |name, *args|
-      @template.content_tag :div, class: "field" do
+      content_tag :div, class: "field" do
         field_label(name, *args) + @template.tag(:br) + super(name, *args)
       end
     end
   end
 
   def check_box(name, *args)
-    @template.content_tag :div, class: "field" do
+    content_tag :div, class: "field" do
       super + ' ' + field_label(name, *args)
+    end
+  end
+
+  def error_messages
+    if object.errors.full_messages.any?
+      content_tag(:div, class: 'error_messages') do
+        content_tag(:h2, 'Invalid Fields') + content_tag(:ul) do
+          object.errors.full_messages.maps do |msg|
+            content_tag(:li, msg)
+          end.join.html_safe
+        end
+      end
     end
   end
 
@@ -22,6 +35,6 @@ class LabeledFormBuilder < ActionView::Helpers::FormBuilder
   end
 
   def objectify_options(options)
-    super.extract(options[:label])
+    super.except(options[:label])
   end
 end
