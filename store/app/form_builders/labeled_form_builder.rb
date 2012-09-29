a24be0994f08a5cc0df5f6c36d2cc67759a6454e
David Silver <dsilver@candidatemetrics.com>
2012-09-28 17:20:11 -0700

    generalize the LabeledFormBuilder

--- store/app/form_builders/labeled_form_builder.rb
+++ store/app/form_builders/labeled_form_builder.rb
@@ -1,7 +1,15 @@
 class LabeledFormBuilder < ActionView::Helpers::FormBuilder
-  def text_field(name, *args)
+  %w[text_field text_area password_field collection_select].each do |method_name|
+    define_method(method_name) do |name, *args|
+      @template.content_tag :div, class: "field" do
+        label(name) + @template.tag(:br) + super(name, *args)
+      end
+    end
+  end
+
+  def check_box(name, *args)
     @template.content_tag :div, class: "field" do
-      label(name) + @template.tag(:br) + super
+      super + ' ' + label(name)
     end
   end
 end
